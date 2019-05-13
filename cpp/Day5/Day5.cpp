5e8caf030aba36d71a4b74f0edb8e242963e9b19
Erik Knaake <e.knaake@student.han.nl>
2019-05-13 21:19:26 +0200

    Day5

---
+++ cpp/Day5/Day5.cpp
@@ -0,0 +1,34 @@
+// Day5.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+
+bool areOpositePools(char a, char b)
+{
+	return a == (b + 32) || a == (b - 32);
+}
+
+int main()
+{
+	std::fstream file("input.txt");
+	std::string line;
+	std::getline(file, line);
+	file.close();
+	int i = 0;
+
+	while (i < line.length() - 1)
+	{
+		if (areOpositePools(line.at(i), line.at(i + 1)))
+		{
+			line.replace(i, 2, "");
+			if (i > 0)
+				i--;
+		}
+		else
+			i++;
+	}
+	std::cout << line << std::endl;
+}
